f0e95b6bc62149224f48923f94c13c9b30e8efdf
appli.kaihatu.jp <appli.kaihatu.jp@gmail.com>
2026-07-13 04:32:56 +0900

    add books to shelf

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,21 @@ export default function Home() {
       {/* 棚全体 */}
       <div className="w-80 h-96 bg-white border-4 border-amber-800 rounded-lg shadow-2xl p-4 flex flex-col gap-4">
         {/* 棚板 1段目 */}
+        <div className="flex gap-2">
+          <div className="w-12 h-20 bg-red-400 rounded-sm"></div>
+          <div className="w-12 h-20 bg-blue-400 rounded-sm"></div>
+          <div className="w-12 h-20 bg-yellow-400 rounded-sm"></div>
+        </div>
+        {/* 棚板 */}
         <div className="w-full h-2 bg-amber-800 rounded-full"></div>
 
         {/* 棚板 2段目 */}
         <div className="w-full h-2 bg-amber-800 rounded-full"></div>
+        {/* 2段目：もっと本が並んでる */}
+        <div className="flex gap-2">
+          <div className="w-12 h-20 bg-green-400 rounded-sm"></div>
+          <div className="w-12 h-20 bg-purple-400 rounded-sm"></div>
+        </div>
 
         {/* 棚板 3段目 */}
         <div className="w-full h-2 bg-amber-800 rounded-full"></div>
